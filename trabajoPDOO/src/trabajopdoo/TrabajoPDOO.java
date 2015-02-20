13bbf3f8ab33d811f59a12b3398c4bcb0913d953
juang3 <juang3@ei140077>
2015-02-20 12:38:53 +0100

    Primera linea, saludo

--- trabajoPDOO/src/trabajopdoo/TrabajoPDOO.java
+++ trabajoPDOO/src/trabajopdoo/TrabajoPDOO.java
@@ -16,6 +16,7 @@ public class TrabajoPDOO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola git");
     }
     
 }
